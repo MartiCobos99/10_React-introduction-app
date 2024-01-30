a611dd7f5d099a8b97034e688ef41f47a02837d5
MartiCobos99 <144819011+MartiCobos99@users.noreply.github.com>
2024-01-30 18:40:38 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const name = "Cobos";
+  return (
+    <>
+      <h1>{`Hola ${name}`} </h1>
+    </>
+  );
+}
+
+export default App
