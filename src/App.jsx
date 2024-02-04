d15175ef129c10a091f9166304be7a5231b99876
MartiCobos99 <144819011+MartiCobos99@users.noreply.github.com>
2024-02-04 06:01:31 +0100

    Finalized Exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Header } from "./header";
+import { Total } from "./total";
+import { Content } from "./content";
 
-function App() {
-  const name = "Cobos";
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
   return (
-    <>
-      <h1>{`Hola ${name}`} </h1>
-    </>
-  );
-}
+    <div>
+      <Header course={course} />
 
-export default App
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
